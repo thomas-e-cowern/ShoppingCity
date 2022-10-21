f4d9b1aaf54b4761fd5e2809703501a147dec172
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-21 08:32:11 -0400

    update NumberFormatter+Extensions with NumberFormatter and currency

--- ShoppingCity/Extensions/NumberFormatter+Extensions.swift
+++ ShoppingCity/Extensions/NumberFormatter+Extensions.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension NumberFormatter {
+    
+    static var currency: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }
+}
