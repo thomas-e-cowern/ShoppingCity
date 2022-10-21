56421445c571bc400a0c99ced3df48218bd45960
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-21 08:26:00 -0400

    add Views folder and ProductRowView

---
+++ ShoppingCity/Views/ProductRowView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductRowView.swift
+//  ShoppingCity
+//
+//  Created by Thomas Cowern on 10/21/22.
+//
+
+import SwiftUI
+
+struct ProductRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductRowView()
+    }
+}
