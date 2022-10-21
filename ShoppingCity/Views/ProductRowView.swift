e79f09c507ca6c10a5fbe71c361a9821f1e27729
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-21 08:50:56 -0400

    add image conditional to ProductRowView and add NoImage to assets

--- ShoppingCity/Views/ProductRowView.swift
+++ ShoppingCity/Views/ProductRowView.swift
@@ -12,9 +12,26 @@ struct ProductRowView: View {
     let product: Product
     
     var body: some View {
-        VStack {
-            Text(product.title)
-            Text(product.price as NSNumber, formatter: NumberFormatter.currency)
+        HStack {
+            AsyncImage(url: URL(string: product.image)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                } else if phase.error != nil {
+                    Image("NoImage")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                } else {
+                    EmptyView()
+                }
+            }
+            .padding()
+            
+            VStack(alignment: .trailing) {
+                Text(product.title)
+                Text(product.price as NSNumber, formatter: NumberFormatter.currency)
+            }
         }
     }
 }
