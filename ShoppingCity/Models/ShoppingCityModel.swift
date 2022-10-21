16c2efdb0430fc2d795280a4ba184abd5c6e09a0
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-21 08:04:01 -0400

    update ShoppingCityModel with initializer, webService and getProducts method

--- ShoppingCity/Models/ShoppingCityModel.swift
+++ ShoppingCity/Models/ShoppingCityModel.swift
@@ -7,4 +7,17 @@
 
 import Foundation
 
-
+class ShoppingCityModel: ObservableObject {
+    
+    let webService: WebService
+    @Published private(set) var products: [Product] = []
+    
+    init(webService: WebService) {
+        self.webService = webService
+    }
+    
+    func getProducts() async throws {
+        products = try await webService.getAllProducts()
+    }
+    
+}
