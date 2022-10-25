9efe70d179ecb7e086b2bec4846bb02f34830bf1
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-25 14:13:03 -0400

    add CategoryView

---
+++ ShoppingCity/Views/CategoryVIEW.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryVIEW.swift
+//  ShoppingCity
+//
+//  Created by Thomas Cowern on 10/25/22.
+//
+
+import SwiftUI
+
+struct CategoryVIEW: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryVIEW_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryVIEW()
+    }
+}
