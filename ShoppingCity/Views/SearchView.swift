573a3ad2adb9761d4dbc67b1393350ebd8af69f7
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-25 13:57:35 -0400

    add NavigationView and SearchBar to SearchView

--- ShoppingCity/Views/SearchView.swift
+++ ShoppingCity/Views/SearchView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State private var searchText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text("Searching for \(searchText)")
+                .searchable(text: $searchText, prompt: "Search our inventory")
+                .navigationTitle("Search for Products!")
+        }
     }
 }
 
