7d3a67b6313e402db7ffb947262cfa2deb667c74
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-28 15:17:40 -0400

    add Button and formatting to LoginView

--- ShoppingCity/Views/LoginView.swift
+++ ShoppingCity/Views/LoginView.swift
@@ -8,29 +8,53 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         VStack {
-            Form {
-                VStack {
-                    Text("Welcome!")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 20)
-                    
-                    Image("store")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 150, height: 150)
-                        .clipped()
-                        .cornerRadius(150)
-                        .padding(.bottom, 30)
-                    
-                    Text("to Shopping City")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 20)
-                }.centerHorizontally()
+            
+            Text("Welcome!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            
+            Image("store")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .clipped()
+                .cornerRadius(150)
+                .padding(.bottom, 30)
+            
+            Text("to Shopping City")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+
+            VStack {
+            TextField("Username", text: $username)
+                .padding()
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+                
+                Text("LOGIN")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
             }
+            .padding()
+            .centerHorizontally()
         }
     }
 }
