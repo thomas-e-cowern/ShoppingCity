7634f77afd78ad74cb9b167f06c55d2d24f67f59
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-28 15:04:36 -0400

    add LoginView

---
+++ ShoppingCity/Views/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  ShoppingCity
+//
+//  Created by Thomas Cowern on 10/28/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Form {
+                TextField
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
